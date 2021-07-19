c059d30b60bf88e213a71a02398f495c8d4ee345
SameerEhsanmund <sameer.baigs321@gmail.com>
2021-07-19 11:16:52 +0500

    Colliders

---
+++ Relentless Fishing/Assets/scripts/hookMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hookMovement : MonoBehaviour
+{
+
+    private CharacterController cc;
+
+    public Vector3 direction;
+
+    public float speed;
+    private float timeToDestroyFish = 1f;
+
+    void Start()
+    {
+        
+
+    }
+
+    public void Update()
+    {
+
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        direction = new Vector3(horizontal, vertical, 0f).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+
+            cc.Move(direction * speed * Time.deltaTime);
+        }
+
+        if (direction.magnitude <= 0.1f)
+        {
+
+            cc.Move(direction * speed * Time.deltaTime);
+        }
+    }
+
+    public void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+
+        if (hit.transform.tag == "fish")
+        {
+
+            Destroy(gameObject.tag == "fish", timeToDestroyFish);
+        }
+    }
+}
