1b86d17adfe542800d2c18e0ad55a6851b29d939
SameerEhsanmund <sameer.baigs321@gmail.com>
2021-07-20 11:38:06 +0500

    Rope Mechanics

---
+++ Relentless Fishing/Assets/scripts/RopeMechanics.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeMechanics : MonoBehaviour
+{
+    
+    void Start()
+    {
+
+        GetComponent<CharacterJoint>().connectedBody = transform.parent.GetComponent<Rigidbody>();
+    }
+}
