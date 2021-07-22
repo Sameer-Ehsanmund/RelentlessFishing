fe41542e333e0089ea97f685e8a7145863b51f62
SameerEhsanmund <sameer.baigs321@gmail.com>
2021-07-22 16:58:58 +0500

    UI

---
+++ Relentless Fishing/Assets/scripts/buttonAnimations.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonAnimations : MonoBehaviour
+{
+
+    public Animator anim;
+
+    public GameObject startBut;
+    public GameObject gameText;
+    public GameObject fishCollectedText;
+    public GameObject FCS;
+
+    public void onGameStart()
+    {
+
+        anim.enabled = true;
+        startBut.active = false;
+        gameText.active = false;
+        
+        StartCoroutine(fishcollectionLabel());
+    }
+
+    IEnumerator fishcollectionLabel()
+    {
+
+        yield return new WaitForSeconds(4);
+        fishCollectedText.active = true;
+    }
+}
--- Relentless Fishing/Assets/scripts/fishCollectionSystem.cs
+++ Relentless Fishing/Assets/scripts/fishCollectionSystem.cs
@@ -1,16 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class fishCollectionSystem : MonoBehaviour
 {
 
-    public int fishCollected = 0;
+    public int fishCollected;
+
+    public void Update()
+    {
+
+        OnGUI();
+    }
 
     void OnGUI()
     {
 
         GUI.color = Color.black;
-        GUI.Label(new Rect(20, 20, 100, 50), ("Total Fish: " + fishCollected));
+        GUI.Label(new Rect(20, 20, 100, 50), ("" + fishCollected));
     }
+        
+
 }
