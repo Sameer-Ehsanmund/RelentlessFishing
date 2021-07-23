90635aef7020b0209d2954a7903b618a4d9e2643
SameerEhsanmund <sameer.baigs321@gmail.com>
2021-07-23 12:29:35 +0500

    ROPE

---
+++ Relentless Fishing/Assets/scripts/ROPEMECHANICS.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ROPEMECHANICS : MonoBehaviour
+{
+
+    Vector3 mouseWorldPosition;
+    RaycastHit raycastHit;
+ 
+ void Update()
+    {
+        DestroyTile();
+    }
+
+    void DestroyTile()
+    {
+        mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Debug.DrawLine(transform.position, mouseWorldPosition, Color.red);
+    }
+}
